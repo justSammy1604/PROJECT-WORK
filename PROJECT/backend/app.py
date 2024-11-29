69511d7ce20924354d536ebf819604c5bb0da5f2
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-11-29 20:47:44 +0530

    Update app.py

--- PROJECT/backend/app.py
+++ PROJECT/backend/app.py
@@ -3,3 +3,6 @@ import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain import PromptTemplate
 from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+
