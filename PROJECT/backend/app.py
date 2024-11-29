1bb66c010e3b3fdd08582870dc9c127334815192
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-11-29 20:48:55 +0530

    Update app.py

--- PROJECT/backend/app.py
+++ PROJECT/backend/app.py
@@ -1,8 +1,13 @@
 #To add main Langchain code here.
 import os
+from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain import PromptTemplate
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+
+
+
 
