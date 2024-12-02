b190a7826ab5a2087da3492897616158d6c41d35
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-12-02 13:11:59 +0530

    Update app.py

--- PROJECT/backend/app.py
+++ PROJECT/backend/app.py
@@ -10,7 +10,7 @@ from langchain.chains import RetrievalQA
 
 #Have to discuss which vectorDB and model y'all would prefer.
 
-load_dotenv()
+load_dotenv() 
 api_key = os.getenv('GOOGLE_API_MODEL') 
 
 model = ChatGoogleGenerativeAI(model='',google_api_key=api_key, temperature=0.4, convert_system_message_to_human=True) 
