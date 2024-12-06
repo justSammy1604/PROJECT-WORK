ff02362a770d972ef93dc9b3612f1369a72dbf48
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-12-06 10:52:56 +0530

    Update app.py

--- PROJECT/backend/app.py
+++ PROJECT/backend/app.py
@@ -9,11 +9,22 @@ from langchain.vectorstores import Chroma
 from langchain_milvus import Milvus
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA 
-
-#Have to discuss which vectorDB and model y'all would prefer.
-
 load_dotenv() 
 api_key = os.getenv('GOOGLE_API_MODEL') 
-
 model = ChatGoogleGenerativeAI(model='',google_api_key=api_key, temperature=0.4, convert_system_message_to_human=True) 
 
+
+
+def preprocessing(): # Joseph, perform stopword removal on Terrence's data from the crawler here.
+  pass
+
+
+
+def RAG(): #Will add the required functions and definition to create the RAG model.
+  pass
+
+
+def response(): #Adding the response part seperately to be called via the API to the frontend
+  pass
+
+data = 1 # Terrence, you place your Crawler and the text it extracts here in this var.
