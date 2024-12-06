6b1d7ea29494e5c85293200c37a47954b8f2050a
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-12-06 20:00:13 +0530

    Update app.py

--- PROJECT/backend/app.py
+++ PROJECT/backend/app.py
@@ -13,7 +13,9 @@ from langchain.chains import RetrievalQA
 load_dotenv() 
 api_key = os.getenv('GOOGLE_API_MODEL') 
 model = ChatGoogleGenerativeAI(model='',google_api_key=api_key, temperature=0.4, convert_system_message_to_human=True) 
-
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
 
 data = 1 # Terrence, you place your Crawler and the text it extracts here in this var.
 loader = 1 #This is an instance of the document loader
@@ -28,10 +30,20 @@ def preprocessing(docs): # Joseph, perform stopword removal on Terrence's data f
   return clean_data
 
 
-def rag_model():  #Will add the required functions and definition to create the RAG model.
-  documents = 
+def split_text():  #Will add the required functions and definition to create the RAG model.
   text_split = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
   docs = text_split.split_documents(documents)
+  return docs
+
+def vectordb_information():
+  vectorstore = Milvus.from_documents(  # or Zilliz.from_documents
+    documents=docs,
+    embedding=embeddings,
+    connection_args={
+        "uri": "./milvus_demo.db",
+    },
+    drop_old=True,  # Drop the old Milvus collection if it exists
+)
 
   
 
