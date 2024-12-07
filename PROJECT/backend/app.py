7579abf08e6ae79208070d6cd3d3653aad66192f
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-12-07 09:31:08 +0530

    Update app.py

--- PROJECT/backend/app.py
+++ PROJECT/backend/app.py
@@ -38,6 +38,11 @@ def vectordb_information(docs):
     return vectorstore
 
 def rag_model(vectorstore):
+    template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
+        {context}
+        Question: {question}
+        Helpful Answer:"""
+    QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
     qa_chain = RetrievalQA.from_chain_type(
         llm=model,
         chain_type="stuff",
