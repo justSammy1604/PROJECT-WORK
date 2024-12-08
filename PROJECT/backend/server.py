fd47a1320c730a7794adb84f0cd3585ba1cdefe6
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-12-08 20:35:50 +0530

    Update server.py

--- PROJECT/backend/server.py
+++ PROJECT/backend/server.py
@@ -3,3 +3,19 @@
 
 from flask import Flask, request, jsonify
 from backend.app import rag_pipeline, response
+
+@app.route('/query',methods=['POST'])
+def query():
+  try:
+    data = request.json
+    query = data.get('query')
+    if not query:
+      return jsonify({'error':'Query Not Provided'}),400
+    answer = response(query,rag_chain)
+    return jsonify({'answer':answer})
+  except:
+     return jsonify({"error": str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4200, debug=True)
