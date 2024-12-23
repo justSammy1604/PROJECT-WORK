7a7427685c4a52020b0f9980fcb22f9f6b84e80a
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2024-12-23 07:58:07 +0530

    Update app.py

--- PROJECT/backend/app.py
+++ PROJECT/backend/app.py
@@ -17,7 +17,7 @@ import spacy
 load_dotenv() 
 api_key = os.getenv('GOOGLE_API_MODEL') 
 model = ChatGoogleGenerativeAI(model='gemini-1.5-pro',google_api_key=api_key, temperature=0.4, convert_system_message_to_human=True) 
-embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=api_key)
+embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=api_key) 
 
 
 
