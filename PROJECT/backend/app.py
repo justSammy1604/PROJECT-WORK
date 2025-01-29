571926f2e8cd3fb8c0e3a08ff358ddb7d27d247f
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2025-01-29 10:13:14 +0530

    Update app.py

--- PROJECT/backend/app.py
+++ PROJECT/backend/app.py
@@ -5,7 +5,7 @@ import numpy as np
 from datetime import datetime 
 import redis
 from dotenv import load_dotenv 
-from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings 
 from langchain_core.prompts import PromptTemplate 
 from langchain.chains.question_answering import load_qa_chain 
 from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader, DirectoryLoader
