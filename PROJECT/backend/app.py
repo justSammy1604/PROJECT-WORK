3ed295a7215292f15a459447f08a42da52df445f
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2025-02-05 10:49:26 +0530

    Update app.py

--- PROJECT/backend/app.py
+++ PROJECT/backend/app.py
@@ -87,17 +87,19 @@ def rag_model(vectorstore):
 def query_response(query, rag_chain):
   try:
     query_embedding = embedding_model.embed_query(query)
-    cached_response = cache.get_similar(query_embedding)
-    if cached_response:
-        print('Cache Hit!!')
-        return cached_response
+    redis_docs = redis_vectorstore.similarity_search_by_vector(query_embedding, k=1)
+    if redis_docs:
+        print("Cache Hit!!")
+        return redis_docs[0].page_content
     
     print('Cache Miss!!')
     result = rag_chain({"query": query})
     response = result['result']
 
-    cache.add_entry(query_embedding,response)
-    return response
+    redis_vectorstore.add_texts([query], [response])
+
+    #for quick lookup
+    redis_client.setex(f"response:{query}", cache_ttl, response)
   except Exception as e:
     return f"An error occurred: {str(e)}"
 
