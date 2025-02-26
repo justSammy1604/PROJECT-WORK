06be05772d603ec9324c39ce883bd74bd7872058
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2025-02-26 18:38:53 +0530

    Update expt.py

--- Experiement_code/expt.py
+++ Experiement_code/expt.py
@@ -2,6 +2,7 @@ import os
 import time
 from dotenv import load_dotenv 
 from langchain_cohere import ChatCohere, CohereEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_core.prompts import PromptTemplate 
 from langchain.chains import RetrievalQA 
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
@@ -16,6 +17,9 @@ api_key = os.getenv('COHERE_API_KEY')
 # Initialize model and embedding
 model = ChatCohere(model='command-r-plus', cohere_api_key=api_key, temperature=0.4, convert_system_message_to_human=True)
 embedding_model = CohereEmbeddings(model="embed-multilingual-v2.0", cohere_api_key=api_key)
+
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash",google_api_key=api_key, temperature=0.4, convert_system_message_to_human=True) 
+embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=api_key) 
 
 # Load and process documents
 data = 'pdfdata'
