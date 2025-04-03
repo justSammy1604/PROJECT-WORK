bda3d7d387f67dea40886f9a8a78c86ffaa3c77e
Samuel Mesquita <thesammesquita16049@gmail.com>
2025-04-03 20:03:18 +0530

    Added_link_route

--- PROJECT/backend/server.py
+++ PROJECT/backend/server.py
@@ -1,13 +1,54 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS 
+import requests
 from app import rag_pipeline, SemanticCache
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
 CORS(app)  # This enables CORS for all routes
 
 data = 'crawled_data'  # We can also add crawled_data file as input here. 
 rag_chain = rag_pipeline(data)
 cache = SemanticCache()
+@app.route('/links', methods=['GET'])
+def get_top_links(search):
+    url = "https://api.scrapingdog.com/google"
+    params = {
+    "api_key": os.getenv("SCRAP_KEY"),
+    "query": search,
+    "results": 20,
+    "country": "us",
+    "page": 0,
+    "advance_search": "false"
+}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+    
+    links = []
+    if "organic_results" in data:
+        for result in data["organic_results"]:
+            if "link" in result:
+                links.append(result["link"])
+
+    if "menu_items" in data:
+        for item in data["menu_items"]:
+            if "link" in item:
+                links.append(item["link"])
+
+    if "peopleAlsoAskedFor" in data:
+        for question in data["peopleAlsoAskedFor"]:
+          if "link" in question:
+            links.append(question["link"])
+
+    if "relatedSearches" in data:
+        for related in data['relatedSearches']:
+            if "link" in related:
+                links.append(related["link"])
+
+    return links
+
 @app.route('/query', methods=['POST'])
 def query():
     try:
