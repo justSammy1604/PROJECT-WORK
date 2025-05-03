4698abed1aa692fce18fd7902fe518f231d1c0ce
Terrence Braganca <113404658+Terrence31@users.noreply.github.com>
2025-05-03 12:19:54 +0530

    Update crawler.py

--- PROJECT/backend/crawler.py
+++ PROJECT/backend/crawler.py
@@ -102,11 +102,11 @@ async def crawl_parallel(urls: List[str], searchText, max_concurrent: int = 3):
                     fail_count += 1
                 elif result.success:
                     success_count += 1
-                    formatted_text = clean_text_with_ai(result.markdown_v2.raw_markdown)
+                    #formatted_text = clean_text_with_ai(result.markdown_v2.raw_markdown)
                     # Save the crawled content to a file
                     file_name = f"crawled_data/{searchText.replace(' ', '_')}.txt"
                     with open(file_name, "a", encoding="utf-8") as file:
-                        file.write(formatted_text)
+                        file.write(result.markdown_v2.raw_markdown)
                     print(f"Saved content to: {file_name}")
                 else:
                     fail_count += 1
